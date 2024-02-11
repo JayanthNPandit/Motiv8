6fabeb920a40d2fd37672ebe34e706d2f1d733bd
Yash Kukrecha <ykukrecha@gmail.com>
2024-02-11 14:38:01 -0600

    created the framework for the project

---
+++ backend/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArsEaBAvKiFx3ude4_ROkBY5Z3Nt4ZbKA",
+  authDomain: "group-goals-b006d.firebaseapp.com",
+  projectId: "group-goals-b006d",
+  storageBucket: "group-goals-b006d.appspot.com",
+  messagingSenderId: "995025574167",
+  appId: "1:995025574167:web:1ee404f2e7c500d7f5bf52",
+  measurementId: "G-XGQPZ1WJ6G"
+};
+
+const app = initializeApp(firebaseConfig);
