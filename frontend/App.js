43a3a1be4b1a4c98024c90d053ddefaf33b1063a
Vincent Do <vincentvietdo@gmail.com>
2024-03-31 12:45:47 -0500

    poppins font now pops off correctly

--- frontend/App.js
+++ frontend/App.js
@@ -1,37 +1,92 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import UploadPhotoScreen from './screens/UploadPhotoScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import LoginScreen from './screens/LoginScreen';
-import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import OnboardingScreen from './screens/OnboardingScreen';
-import GroupsScreen from './screens/GroupsScreen';
-import CreateGroupScreen from './screens/CreateGroupScreen';
-import JoinGroupScreen from './screens/JoinGroupScreen';
-import ConfirmGroupScreen from './screens/ConfirmGroupScreen';
-import GroupCodeScreen from './screens/GroupCodeScreen';
-import AddGoalScreen from './screens/AddGoalScreen';
-import GoalsScreen from './screens/GoalsScreen';
+import UploadPhotoScreen from "./screens/UploadPhotoScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ForgotPasswordScreen from "./screens/ForgotPasswordScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import GroupsScreen from "./screens/GroupsScreen";
+import CreateGroupScreen from "./screens/CreateGroupScreen";
+import JoinGroupScreen from "./screens/JoinGroupScreen";
+import ConfirmGroupScreen from "./screens/ConfirmGroupScreen";
+import GroupCodeScreen from "./screens/GroupCodeScreen";
+import AddGoalScreen from "./screens/AddGoalScreen";
+import GoalsScreen from "./screens/GoalsScreen";
 
 import { AuthProvider } from "./contexts/AuthContext";
 import { useAuth } from "./contexts/AuthContext";
-import { Image } from 'react-native';
-import home from './assets/home.png';
-import goal from './assets/goal.png';
-import post from './assets/post.png';
-import group from './assets/group.png';
-import profile from './assets/user.png';
+import { Image } from "react-native";
+import home from "./assets/home.png";
+import goal from "./assets/goal.png";
+import post from "./assets/post.png";
+import group from "./assets/group.png";
+import profile from "./assets/user.png";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// Prevent native splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
+
+async function loadFonts() {
+  await Font.loadAsync({
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-BlackItalic": require("./assets/fonts/Poppins-BlackItalic.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-BoldItalic": require("./assets/fonts/Poppins-BoldItalic.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraBoldItalic": require("./assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraLightItalic": require("./assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    "Poppins-Italic": require("./assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-LightItalic": require("./assets/fonts/Poppins-LightItalic.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-MediumItalic": require("./assets/fonts/Poppins-MediumItalic.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-SemiBoldItalic": require("./assets/fonts/Poppins-SemiBoldItalic.ttf"),
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ThinItalic": require("./assets/fonts/Poppins-ThinItalic.ttf"),
+  });
+}
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadFonts();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (appIsReady) {
+      // Hide the splash screen once the app is done loading
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null; // what is displayed while the app is loading. could be a loading symbol or a screen, up to y'all
+  }
+
   return (
     <AuthProvider>
       <AppContent />
@@ -40,36 +95,82 @@ export default function App() {
 }
 
 function AppContent() {
-  const { user } = useAuth(); 
+  const { user } = useAuth();
   return (
-     /*
-     <NavigationContainer>
-       <Stack.Navigator initialRouteName={!user ? "Welcome" : "Profile"}>
-         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="Groups" component={GroupsScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="CreateGroup" component={CreateGroupScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="JoinGroup" component={JoinGroupScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="ConfirmGroup" component={ConfirmGroupScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="GroupCode" component={GroupCodeScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="Feed" component={UploadPhotoScreen} options={{headerShown: false}}/>
-         <Stack.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}/>
-       </Stack.Navigator>
+    /*
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={!user ? "Welcome" : "Profile"}>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Groups"
+          component={GroupsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateGroup"
+          component={CreateGroupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="JoinGroup"
+          component={JoinGroupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ConfirmGroup"
+          component={ConfirmGroupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="GroupCode"
+          component={GroupCodeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Feed"
+          component={UploadPhotoScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
     */
 
     // only show the goals screen for now
-    
 
     // <NavigationContainer>
     //     <Stack.Screen name="Goals" component={GoalsScreen} options={{headerShown: false}}/>
     //     <Stack.Screen name="AddGoals" component={AddGoalScreen} options={{headerShown: false}}/>
     // </NavigationContainer>
 
-  
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({route}) => ({
